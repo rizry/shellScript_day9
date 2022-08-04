78d9b86b4941b0a629404ec8727cd290270cf34b
rizry <rizzwan.ansarii@gmail.com>
2022-08-04 19:56:23 +0530

    added 100 hrs or 20 days to work

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,19 +4,19 @@ isFullTime=1;
 isPartTime=2;
 empRatePerHr=20;
 
-workingDays=20;
+daysToWork=20;
+hrsToWork=100
 empHrs=0;
-while [ $workingDays -gt 0 ]; do
+while [ $daysToWork -gt 0 -a $empHrs -lt $hrsToWork ]; do
   
   randomCheck=$((1+RANDOM%3));
-  
   case "$randomCheck" in
     $isFullTime)
-      ((empHrs+=8))
-      ((workingDays--));;
+      ((empHrs+=8));
+      ((daysToWork--));;
     $isPartTime)
-      ((empHrs+=4))
-      ((workingDays--));;
+      ((empHrs+=4));
+      ((daysToWork--));;
     *)
       ((empHrs+=0));;
   esac

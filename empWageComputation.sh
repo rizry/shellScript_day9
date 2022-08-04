0af2d29e0c1d8c10e2ee7c6788b8802491cc0d98
rizry <rizzwan.ansarii@gmail.com>
2022-08-04 20:46:54 +0530

    stored daily wage and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,6 @@
 printf "Welcome to Employee Wage Computation Program. \n\n"
 
-function getWorkHrs(){
+function getEmpWage(){
   isFullTime=1;
   isPartTime=2;
   empRatePerHr=20;
@@ -8,26 +8,36 @@ function getWorkHrs(){
   daysToWork=20;
   hrsToWork=100
   empWorkHrs=0;
+
+  dailyWageArr=()
+
   while [ $daysToWork -gt 0 -a $empWorkHrs -lt $hrsToWork ]; do
     
     randomCheck=$((1+RANDOM%3));
     case "$randomCheck" in
       $isFullTime)
         ((empWorkHrs+=8));
-        ((daysToWork--));;
+        ((daysToWork--));
+        dailyWageArr+=( $(( $empRatePerHr * 8 )) );;
       $isPartTime)
         ((empWorkHrs+=4));
-        ((daysToWork--));;
+        ((daysToWork--));
+        dailyWageArr+=( $(( $empRatePerHr * 4 )) );;
       *)
-        ((empWorkHrs+=0));;
+        ((empWorkHrs+=0));
+        dailyWageArr+=(0);;
     esac
 
   done
-
+   
   monthlySalary=$((empRatePerHr * empWorkHrs));
-  echo "$empWorkHrs"
+
+  echo "${dailyWageArr[*]}" 
+  echo "$monthlySalary"  
 
 }
 
-work_hours=$(getWorkHrs)
+{ read -r dailyWageArr 
+  read -r totalWage
+} <<< $(getEmpWage)
 

4e92a86bed7516b978045b06e7afe8ec83242854
rizry <rizzwan.ansarii@gmail.com>
2022-08-04 19:39:35 +0530

    added monthly wage calculations

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,15 +3,27 @@ printf "Welcome to Employee Wage Computation Program. \n\n"
 isFullTime=1;
 isPartTime=2;
 empRatePerHr=20;
-randomCheck=$((1+RANDOM%3));
 
-case "$randomCheck" in
-  $isFullTime)
-    empHrs=8;;
-  $isPartTime)
-    empHrs=4;;
-  *)
-    empHrs=0;;
-esac
 
-salary=$((empRatePerHr * empHrs));
+
+
+workingDays=20;
+empHrs=0;
+while [ $workingDays -gt 0 ]; do
+  
+  randomCheck=$((1+RANDOM%3));
+  
+  case "$randomCheck" in
+    $isFullTime)
+      ((empHrs+=8))
+      ((workingDays--));;
+    $isPartTime)
+      ((empHrs+=4))
+      ((workingDays--));;
+    *)
+      ((empHrs+=0));;
+  esac
+
+done
+
+monthlySalary=$((empRatePerHr * empHrs));

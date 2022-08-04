3d1d788b0656882cf85c6f4f39dbb9a24f3ee2b1
rizry <rizzwan.ansarii@gmail.com>
2022-08-04 20:04:37 +0530

    added function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,26 +1,33 @@
 printf "Welcome to Employee Wage Computation Program. \n\n"
 
-isFullTime=1;
-isPartTime=2;
-empRatePerHr=20;
+function getWorkHrs(){
+  isFullTime=1;
+  isPartTime=2;
+  empRatePerHr=20;
 
-daysToWork=20;
-hrsToWork=100
-empHrs=0;
-while [ $daysToWork -gt 0 -a $empHrs -lt $hrsToWork ]; do
-  
-  randomCheck=$((1+RANDOM%3));
-  case "$randomCheck" in
-    $isFullTime)
-      ((empHrs+=8));
-      ((daysToWork--));;
-    $isPartTime)
-      ((empHrs+=4));
-      ((daysToWork--));;
-    *)
-      ((empHrs+=0));;
-  esac
+  daysToWork=20;
+  hrsToWork=100
+  empWorkHrs=0;
+  while [ $daysToWork -gt 0 -a $empWorkHrs -lt $hrsToWork ]; do
+    
+    randomCheck=$((1+RANDOM%3));
+    case "$randomCheck" in
+      $isFullTime)
+        ((empWorkHrs+=8));
+        ((daysToWork--));;
+      $isPartTime)
+        ((empWorkHrs+=4));
+        ((daysToWork--));;
+      *)
+        ((empWorkHrs+=0));;
+    esac
 
-done
+  done
 
-monthlySalary=$((empRatePerHr * empHrs));
+  monthlySalary=$((empRatePerHr * empWorkHrs));
+  echo "$empWorkHrs"
+
+}
+
+work_hours=$(getWorkHrs)
+
